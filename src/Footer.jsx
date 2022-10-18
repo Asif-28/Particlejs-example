f3cbf64a42782b6dff7b62adb31c9653932832be
Asif Ali <asif.a.7277@gmail.com>
2022-10-18 19:43:04 +0530

    added framer motion

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,11 +1,40 @@
 import React from "react";
+import { motion, useScroll, useSpring } from "framer-motion";
+import AOS from "aos";
+import "aos/dist/aos.css"; // You can also use <link> for styles
+// ..
 import "./Footer.css";
+import Text from "./components/Text";
+AOS.init();
 const Footer = () => {
+  const { scrollYProgress } = useScroll();
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+    restDelta: 0.002,
+  });
   return (
-    <div>
-      <h1 className="container ">
-        zdfsz gfgfgh fghf ghf hgfgh ghed56 dfytdf56 fcrtfghfr drd
-      </h1>
+    <div className="cont">
+      <motion.div className="progress-bar" style={{ scaleX }} />
+      <div
+        data-aos="fade-right"
+        data-aos-offset="100"
+        data-aos-easing="ease-in-sine"
+        data-aos-duration="1000"
+      >
+        <h1>
+          <code>useScroll</code> with spring smoothing
+        </h1>
+      </div>
+      <div
+        data-aos="fade-right"
+        data-aos-offset="100"
+        data-aos-easing="ease-in-sine"
+        data-aos-duration="1000"
+        className="text"
+      >
+        <Text />
+      </div>
     </div>
   );
 };
